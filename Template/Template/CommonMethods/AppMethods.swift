dfa651135e1101f935d10d6c684f0cdbb64535e8
Martin Prabhu <martinprabhum@tag049.tag.dc>
2019-03-12 22:01:56 +0530

    Added the new files

---
+++ Template/Template/CommonMethods/AppMethods.swift
@@ -0,0 +1,53 @@
+//
+//  AppMethods.swift
+//  Template
+//
+//  Created by Martin Prabhu on 11/03/19.
+//  Copyright © 2019 TAG. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+import UIKit
+
+class AppMethods: NSObject {
+
+    public static let shared = AppMethods()
+
+    func isInternetAvailable() -> Bool{
+        var zeroAddress = sockaddr_in()
+            zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+            zeroAddress.sin_family = sa_family_t(AF_INET)
+
+    let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+        $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+            SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+        }
+    }
+    var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+        return false
+        }
+            let isReachable = flags.contains(.reachable)
+            let needsConnection = flags.contains(.connectionRequired)
+            return (isReachable && !needsConnection)
+    }
+
+}
+
+
+
+extension UIViewController {
+    func displayAlert(title: String, message: String, options: String..., completion: @escaping (Int) -> Void) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        for (index, option) in options.enumerated() {
+            alertController.addAction(UIAlertAction.init(title: option, style: .default, handler: { (action) in
+                completion(index)
+            }))
+        }
+        self.present(alertController, animated: true, completion: nil)
+    }
+    func dismisssAlert(){
+    }
+
+}
---
+++ Template/Template/StringConstants/CommonStrings.swift
@@ -0,0 +1,33 @@
+//
+//  CommonStrings.swift
+//  Template
+//
+//  Created by Martin Prabhu on 11/03/19.
+//  Copyright © 2019 TAG. All rights reserved.
+//
+
+import Foundation
+
+struct StringConstant {
+
+    public static let shared = StringConstant()
+
+    static let internetError = "Please check your internet connectivity"
+    static let internetSuccess = "Internet Connection available"
+
+    static let invalidUsername = "Username is invalid"
+    static let invalidPassword = "Password is invalid"
+
+    static let emptyUsername = "Username is empty"
+    static let emptyPassword = "Password is empty"
+
+    static let invalidEmail = "Email is not valid"
+    static let emptyEmail = "Email is empty"
+
+
+
+
+
+
+
+}
